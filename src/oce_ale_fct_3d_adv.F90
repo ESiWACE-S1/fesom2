2d48521435ffe7de6218b119eb2ce033a9e6a7ed
gvdoord <g.vandenOord@gmail.com>
2020-05-10 22:56:53 +0200

    Changed to use inverse area in C impl

--- src/oce_ale_fct_3d_adv.F90
+++ src/oce_ale_fct_3d_adv.F90
@@ -553,7 +553,7 @@ subroutine fct_ale(ttf, iter_yn, mesh)
 
     ! Insert call to first C-function here
     call fct_ale_pre_comm(alg_state, fct_ttf_max, fct_ttf_min, fct_plus, fct_minus,&
-                          tvert_max, tvert_min, ttf, fct_LO, fct_adf_v, fct_adf_h, UV_rhs, area,& 
+                          tvert_max, tvert_min, ttf, fct_LO, fct_adf_v, fct_adf_h, UV_rhs, area_inv,& 
                           myDim_nod2D, eDim_nod2D, myDim_elem2D, myDim_edge2D, nl,&
                           nlevels_nod2D, nlevels, elem2D_nodes, nod_in_elem2D_num, nod_in_elem2D,&
                           size(nod_in_elem2D, 1), edges, edge_tri, vlimit, flux_eps, bignumber, dt)
